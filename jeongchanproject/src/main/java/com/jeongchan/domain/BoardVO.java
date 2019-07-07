c4ee8c2ac447b18e9b2a4d1925ae463148a9b83b
leejeongchan <dlwjdcks5343@naver.com>
2019-07-07 19:06:11 +0900

    주석처리

--- jeongchanproject/src/main/java/com/jeongchan/domain/BoardVO.java
+++ jeongchanproject/src/main/java/com/jeongchan/domain/BoardVO.java
@@ -4,11 +4,21 @@ import java.util.Date;
 import java.util.List;
 
 import lombok.Data;
-
+/*
+ * @Auth : Lee Jeong Chan
+ * bno: board number
+ * content: board content
+ * writer: board writer
+ * viewcnt: if you enter specific board item, view Count is increased by user
+ * regdate: the date register post board
+ * updateDate: the date register modify board
+ * replycnt: in specific board , the reply count registered by user
+ * filecnt: in specific board , the attached file count
+ */
 @Data
 public class BoardVO {
-	private Long bno;
-	private String title;
+	private Long bno; 
+	private String title; 
 	private String content;
 	private String writer;
 	private Long viewcnt;
